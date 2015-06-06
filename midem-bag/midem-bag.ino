ac63652135252d7cc53048b39042e9c587a09814
theleadingzero <becky@theleadingzero.com>
2015-06-06 11:42:58 +0200

    Basic commands sent from phone

--- midem-bag/midem-bag.ino
+++ midem-bag/midem-bag.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
-#include <WS2801.h>
+#include "WS2801.h"
+#include "Adafruit_BLE_UART.h"
 
 /*****************************************************************************
 Example sketch for driving WS2801 pixels
@@ -12,39 +13,106 @@ Example sketch for driving WS2801 pixels
 int dataPin = 4;
 int clockPin = 5;
 
-int micPin = A0;
-// Don't forget to connect the ground wire to Arduino ground,
-// and the +5V wire to a +5V supply
+// Connect CLK/MISO/MOSI to hardware SPI
+// e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 2     // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RST 9
 
+Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
+aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
 
 // Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
 WS2801 strip = WS2801(48, dataPin, clockPin);
 
-// Optional: leave off pin numbers to use hardware SPI
-// (pinout is then specific to each board and can't be changed)
-//WS2801 strip = WS2801(25);
 
 void setup() {
-
+  // LED set up
   strip.begin();
 
   // Update LED contents, to start they are all 'off'
   strip.show();
 
-  colorWipe(Color(255, 0, 0), 50);
-  Serial.begin( 9600 );
+  // start Serial
+  Serial.begin(9600);
+  while (!Serial); // Leonardo/Micro should wait for serial init
+  Serial.println(F("MIDEM HACK BAG"));
+
+  // start Bluetooth
+  BTLEserial.setDeviceName("BAG"); /* 7 characters max! */
+  BTLEserial.begin();
 }
 
 
 void loop() {
   // Some example procedures showing how to display to the pixels
-
-
-  colorWipe(Color(0, 255, 0), 50);
-  colorWipe(Color(0, 0, 255), 50);
+  //colorWipe(Color(0, 255, 0), 50);
+  //colorWipe(Color(0, 0, 255), 50);
   //rainbow(20);
   //rainbowCycle(20);
 
+  // Tell the nRF8001 to do whatever it should be working on.
+  BTLEserial.pollACI();
+
+  // Ask what is our current status
+  aci_evt_opcode_t status = BTLEserial.getState();
+  // If the status changed....
+  if (status != laststatus) {
+    // print it out!
+    if (status == ACI_EVT_DEVICE_STARTED) {
+      Serial.println(F("* Advertising started"));
+    }
+    if (status == ACI_EVT_CONNECTED) {
+      Serial.println(F("* Connected!"));
+    }
+    if (status == ACI_EVT_DISCONNECTED) {
+      Serial.println(F("* Disconnected or advertising timed out"));
+    }
+    // OK set the last status change to this one
+    laststatus = status;
+  }
+
+  if (status == ACI_EVT_CONNECTED) {
+    // Lets see if there's any data for us!
+    if (BTLEserial.available()) {
+      Serial.print("* "); Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
+    }
+    // OK while we still have something to read, get a character and print it out
+    while (BTLEserial.available()) {
+      char c = BTLEserial.read();
+      Serial.print(c);
+
+      // red if received a 'r'
+      if ( c == 'R' ) {
+        colorWipe(Color(255, 0, 0), 50);
+      }
+
+      // green if received a 'g'
+      if ( c == 'G' ) {
+        colorWipe(Color(0, 255, 0), 50);
+      }
+    }
+    /*
+
+    // Next up, see if we have any data to get from the Serial console
+
+    if (Serial.available()) {
+      // Read a line from Serial
+      Serial.setTimeout(100); // 100 millisecond timeout
+      String s = Serial.readString();
+
+      // We need to convert the line to bytes, no more than 20 at this time
+      uint8_t sendbuffer[20];
+      s.getBytes(sendbuffer, 20);
+      char sendbuffersize = min(20, s.length());
+
+      Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+
+      // write the data
+      BTLEserial.write(sendbuffer, sendbuffersize);
+    }
+    */
+  }
 }
 
 void rainbow(uint8_t wait) {
